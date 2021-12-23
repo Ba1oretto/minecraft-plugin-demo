6839913cd625b82a8c2380520894fa8c084d1a4e
Ba1oretto <sunjiamu@outlook.com>
2021-12-23 18:20:24 +0900

    custom command

---
+++ InstanceAccess/src/main/java/com/plugins/instanceaccess/listeners/DeathListener.java
@@ -0,0 +1,29 @@
+package com.plugins.instanceaccess.listeners;
+
+import com.plugins.instanceaccess.InstanceAccess;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class DeathListener implements Listener {
+
+    private final InstanceAccess instanceAccess;
+
+    public DeathListener(InstanceAccess instanceAccess) {
+        this.instanceAccess = instanceAccess;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent playerDeathEvent) {
+        playerDeathEvent.getPlayer().sendMessage(ChatColor.RED + "你寄了");
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                System.out.println("测试延迟运行");
+            }
+//        }.runTaskLater(InstanceAccess.getPlugin(), 20L);
+        }.runTaskLater(instanceAccess, 20L);
+    }
+}
