30ddc6029c73000fc73448184addb83814d57135
Ba1oretto <sunjiamu@outlook.com>
2021-12-28 19:59:38 +0900

    GUI Menus

---
+++ GUIMenus/src/main/java/com/plugins/guimenus/GUIMenus.java
@@ -0,0 +1,12 @@
+package com.plugins.guimenus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GUIMenus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
