0919755bab0b829cbcf2e8c22e9af4bd68104423
Ba1oretto <sunjiamu@outlook.com>
2022-04-26 10:56:13 +0900

    21-holograms

---
+++ 21-holograms/src/main/java/minecraft/demo/holograms/Holograms.java
@@ -0,0 +1,12 @@
+package minecraft.demo.holograms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Holograms extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
