c27102f6ea7176dcba21354868d45acf2dcb7b34
Ba1oretto <sunjiamu@outlook.com>
2022-04-27 19:33:48 +0900

    28-mongoDB-and-mLab (not useful)

---
+++ 28-mongoDB-and-mLab/src/main/java/minecraft/demo/mongodbandmlab/MongoDBAndMLab.java
@@ -0,0 +1,12 @@
+package minecraft.demo.mongodbandmlab;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MongoDBAndMLab extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
