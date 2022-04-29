eac6425fcb0e41267ac977e724ee8617d2d24e16
Ba1oretto <sunjiamu@outlook.com>
2022-04-29 20:02:22 +0900

    41-staff-home-p2

---
+++ 41-staff-home-p2/src/main/java/minecraft/demo/staffhomep2/StaffHomeP2.java
@@ -0,0 +1,12 @@
+package minecraft.demo.staffhomep2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffHomeP2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    // see staff home part 1
+    }
+
+}
